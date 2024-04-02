07b7f56e21fa1f2beab7d0f4ffc4d6751888ac62
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-04-02 19:10:55 +0700

    ADD: main project - splash screen

---
+++ Projects/Main_Project/BackgroundImageGenerator/constants/styles.js
@@ -0,0 +1,14 @@
+export const GlobalStyles = {
+    colors: {
+        primary100: '#CAF0F8',
+        primary200: '#90E0EF',
+        primary300: '#00B4D8',
+        primary400: '#0077B6',
+        primary500: '#03045E',
+        accent500: '#f7bc0c',
+        error50: '#fcc4e4',
+        error500: '#9b095c',
+        gray500: '#39324a',
+        gray700: '#221c30',
+    },
+};
