7288be9efea15559e996b5dce7b7f4a5f5564e9a
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-04-09 17:49:03 +0700

    COMPLETE: test_backend

--- Projects/Test_Backend/main.py
+++ Projects/Test_Backend/main.py
@@ -1,5 +1,6 @@
 import requests
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 base_url = "https://dummyjson.com/carts"
@@ -10,3 +11,11 @@ async def root():
     response = requests.get(base_url)
     results = response.json()
     return results
+
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
