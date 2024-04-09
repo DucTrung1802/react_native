95a91f0995718556edf00d962dbf3f48d0776abb
Duc Trung <trung.lyduc18@gmail.com>
2024-04-09 21:56:32 +0700

    CHECKPOINT: POST method

--- Projects/Test_Backend/main.py
+++ Projects/Test_Backend/main.py
@@ -1,9 +1,14 @@
-import requests
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
 base_url = "https://dummyjson.com/carts"
+
+
+class UserCreate(BaseModel):
+    user_id: int
+    username: str
 
 
 @app.get("/")
@@ -12,9 +17,16 @@ async def root():
     # results = response.json()
     return "HELLO CLIENT"
 
-@app.post("/post_image")
-async def receive_image(request):
-    print(request)
+
+@app.post("/post_image/")
+async def receive_image(user_data: UserCreate):
+    user_id = user_data.user_id
+    username = user_data.username
+    return {
+        "msg": "we got data succesfully",
+        "user_id": user_id,
+        "username": username,
+    }
 
 
 def main():
